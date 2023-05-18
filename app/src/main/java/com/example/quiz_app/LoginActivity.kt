78e9ee420ffca793112c4056742af0cdf3a8db69
mjanusz01 <mikolajjanusz01@gmail.com>
2023-05-18 09:46:03 +0200

    Fix up 2

--- app/src/main/java/com/example/quiz_app/LoginActivity.kt
+++ app/src/main/java/com/example/quiz_app/LoginActivity.kt
@@ -18,7 +18,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        Log.v("ELO","TEST")
+
         //TODO: połączenie z bazą danych
     }
 
