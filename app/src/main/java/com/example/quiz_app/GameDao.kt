9cb98532bf3187a369712d728a6a697d333da355
mjanusz01 <mikolajjanusz01@gmail.com>
2023-06-04 20:52:06 +0200

    fix database 2

--- app/src/main/java/com/example/quiz_app/GameDao.kt
+++ app/src/main/java/com/example/quiz_app/GameDao.kt
@@ -3,25 +3,6 @@ package com.example.quiz_app
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
-
-
-//@Dao
-//interface GameDao {
-//
-//    @Upsert
-//    suspend fun upsertContact(contact: Question)
-//
-//    @Delete
-//    suspend fun deleteContact(contact: Question)
-//
-//    @Query("SELECT count() FROM Stats where ID = :user_id")
-//    fun getStatsForUser(user_id: Int?): LiveData<Stats?>?
-//
-//    @Transaction
-//    fun getStatsForUser(user: User): LiveData<Stats?>? {
-//        return getStatsForUser(user.id)
-//    }
-//}
 @Dao
 interface QuestionDao {
     @Insert
